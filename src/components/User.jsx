4ff5656514d1edd38369177eaa2d114289f542f8
aziz-codes <azizisoffline@gmail.com>
2022-12-31 16:12:51 +0500

    user image

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -5,7 +5,7 @@ const User = ({users}) => {
 <>
 {users.map((user,index)=>(
     <div className='h-44 w-44 flex items-center justify-center flex-col border rounded-sm' key={index}>
-        <img src={user.avatar} className="h-14 w-14 rounded-full p-1" alt={user.name}/>
+        <img src={user.avatar} className="h-20 w-20 border rounded-full p-1" alt={user.name}/>
         <h4>{user.name}</h4>
         <p className='text-gray-300'>{user.email}</p>
     </div>
