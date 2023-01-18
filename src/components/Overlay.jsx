1dfc88a41d824f162826937c334a3b2747f14324
aziz-codes <azizisoffline@gmail.com>
2023-01-18 22:52:04 +0500

    open user modal

---
+++ src/components/Overlay.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Overlay = ({user,handleClose}) => {
+    console.log(user);
+  return (
+    <div className='w-full h-screen bg-slate-500 bg-opacity-50 fixed flex top-0 left-0 flex-col items-center justify-center' onClick={()=>{handleClose()}}>
+        <div className='bg-white h-56 w-56 rounded-md flex flex-col items-center justify-center'>
+           <img src={user.avatar} className="h-14 w-14 rounded-full" alt={user.name}/>
+           <h3>{user.name}</h3>
+           <label>{user.email}</label>
+        </div>
+    </div>
+  )
+}
+
+export default Overlay
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,7 +1,10 @@
 import React,{useState,useEffect} from 'react'
+import Overlay from './Overlay';
 
 const User = ({users}) => {
   const [data,setData]=useState([]);
+  const [open, setOpen] = useState(false);
+  const [user, setUser] = useState();
   const handleChange = (e) =>{
    const filteredData = users.filter((user)=>user.name.toLowerCase().includes(e));
    setData(filteredData);
@@ -10,15 +13,20 @@ const User = ({users}) => {
   useEffect(()=>{
       setData(users);
   },[users]);
-
-
+const handleClick = (user)=>{
+  setUser(user);
+  setOpen(true);
+}
+const handleClose = () =>{
+  setOpen(false);
+}
   return (
 <>
 <div className='flex flex-col gap-4'>
   <input type="tex" placeholder='Search' className='border outline-none w-56 p-1 rounded-md' onKeyUp={(e)=>{handleChange(e.target.value)}}/>
 <div className='flex flex-row flex-wrap'>
 {data.map((user,index)=>(
-    <div className='h-44 w-44 flex items-center justify-center flex-col border rounded-sm' key={index}>
+    <div className='h-44 w-44 flex items-center justify-center flex-col border rounded-sm cursor-pointer hover:scale-95 transition-all ease-out' key={index} onClick={()=>{handleClick(user)}}>
         <img src={user.avatar} className="h-20 w-20 border rounded-full p-1" alt={user.name}/>
         <h4>{user.name}</h4>
         <p className='text-gray-300'>{user.email}</p>
@@ -27,6 +35,7 @@ const User = ({users}) => {
 ))}
 </div>
 </div>
+{open && <Overlay user={user} handleClose={handleClose}/>}
 </> 
 
 )
